c729151a8b1744defec4ab79cee2faf460b4d8b2
Jinhyeon Kim <jinhyeon0810@gmail.com>
2023-09-10 16:55:36 +0900

    vercel 배포중 vite top-level await 오류

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   ssr: {
-    noExternal: ['buffer'],
+    noExternal: ["buffer"],
+  },
+  build: {
+    // 빌드 옵션 추가
+    target: "esnext", // 또는 원하는 타겟 환경으로 설정
   },
 });
-
